b81ab869dcd3a00bde8ac2b633f40054229f10bf
Suresh <slalcha1@binghamton.edu>
2017-10-03 00:15:27 -0400

    Added check on argument

--- studentCoursesBackup/src/studentCoursesBackup/driver/Driver.java
+++ studentCoursesBackup/src/studentCoursesBackup/driver/Driver.java
@@ -25,11 +25,11 @@ public class Driver {
 			return;
 		}
 
-		if(args[0].contains("${arg0}") || 
-				args[1].contains("${arg1}") || 
-				args[2].contains("${arg2}") ||
-				args[3].contains("${arg3}") ||
-				args[4].contains("${arg4}")) {
+		if((args[0].trim().length() == 0 || args[0].contains("${arg0}")) || 
+				(args[1].trim().length() == 0 || args[1].contains("${arg1}")) || 
+				(args[2].trim().length() == 0 || args[2].contains("${arg2}")) ||
+				(args[3].trim().length() == 0 || args[3].contains("${arg3}")) ||
+				(args[4].trim().length() == 0 || args[4].contains("${arg4}"))) {
 			System.err.println("Please provide valid number of arguments. 5 Arguments are expected: \n1.Input File \n2.Delete File \n3.Output1 File \n4.Output2 File \n5.Output3 File");
 			System.exit(0);
 			return;
